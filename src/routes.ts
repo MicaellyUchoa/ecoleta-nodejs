00f29b86a9eb49668efcf8e1f6a6beac5b02105b
Micaelly Uchoa <micaellyit@hotmail.com>
2020-06-06 22:26:08 -0300

    feature: verificação de campos por parte do backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 import multer from "multer";
@@ -8,7 +9,26 @@ const upload = multer(multerConfig);
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 routes.get("/items", itemsController.index);
-routes.post("/points", upload.single("image"), pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required(),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  pointsController.create
+);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
